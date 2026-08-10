4d644022c711fb4c69a99c5a62faf4256ff6138c
Aluno Vocacao <nicdrihh@gmail.com>
2026-08-10 15:45:44 -0300

    upgrade

---
+++ tarde_programação_2026.2_nycollas_rodrigues/pensamento_projeto_nycollas_rodrigues.py
@@ -0,0 +1,135 @@
+'''
+Um bloco de comentários.
+
+projeto açaiteria:
+
+PO (como dona do negócio: quero um sistema de vendas na açaiteria que eu possa controlar pedidos e vendas)
+
+QA (como compradora: quero um sistema de alto nivel para meu comércio, para que eu possa pedir meus produtos 
+favoritos de forma rápida e efeiciente)
+
+Tech (como programadora de um sistema de altissimo nivel, para minha açaiteria, para que eu possa desenvolver 
+um software claramente eficiente e de bom acesso)
+
+Dev (como programadora: quero um sistema de vendas para meu negócio, para implementar funcionalidades essenciais
+para atender  melhor minha clientela)
+
+UX (como designer de experiencia do usuário: quero um software de lucros para meu negócio, para que eu possa criar 
+uma interface amigável para as pessoas, garantindo boa experiencia)
+
+IA (como analista de dados: quero um bom sistema de vendas para minha açaiteria, para que eu possa coletar e analisar
+as planilhas, ajudando a identicar padrões de consumo e otimizar vendas e marketing)
+
+ 
+
+'''
+''''''
+um bloco de comentário.
+    
+    projeto açaiteria:
+     
+ PO (como dona do negócio :quero um sistema para minhas vendas na açaiteria,para que eu possa controlar pedidos e vendas)  
+
+QA(como compradora :quero um sistema de alto nivel para meu comércio,para que eu possa pedir meus produtos favoritos de forma rápida e eficiente) 
+
+TECH(como progamadora:quero um sistema de um altissimo nivel,para minha açaiteria,para que eu possa desenvolver um software claramente eficiente e de um bom
+acesso.)
+
+DEV(como progamadora:quero um sistema de vendas para meu negócio!para que eu possa implementar melhores funcionalidades essenciais para atender melhor minha clientela)
+
+UX(como designer de experiencia do usuario:quero um  sftwere de lucros para meu comércio,par que eu possa criar uma interfac amigavel para as pessoas,garantindo)
+boa experiencia.)
+
+IA(como analista de dados:quero um bom sistema de vendas para  minha açaiteria,para que eu possa coletar e analisar as planilhas ,ajudando a identificar padroes
+ de consumo e otimizar vendas e marketing)
+
+# isso é um comentário de linha unica.
+
+print(olá mundo!)
+      
+ ''''''
+
+print('Bem-vindo ao meu sistema açaiteria!\n)
+      
+while True:
+print('-' *48 + '\n')
+print('Bem-Vindo a açaiteria')
+print('1 - Pedir Açai')
+print('2 - Ver Produtos Disponiveis')
+print('3 - Realizar compra')
+print('0 - Sair do Sistema')
+print('\n---------------\n')
+
+opcao__definida = int(input('Digite a opção Desejada: '))
+
+if opcao__definida=1:print('1 - Pedindo Açai...')
+
+elif opcao__definida=2:print('Listando Produtos: 
+(frutas, granola, leite em pó)
+
+Adicionais:
+nutella 5,00
+leite em pó 2,00
+frutas 3,00
+mousse de maracujá 3,00
+paçoca 1,00
+granola 3,00
+
+elif opcao__definida=3: print('Realizando venda...')
+
+copo de açai + cupuaçu 500ml (companhamento de uva e leite condensado)
+quantidade no estoque:120\data de validade:20.01.2027
+
+copo de açai 300ml (banana musse de maracujá e paçoca)
+quantidade no estoque:200\data de validade:04.07.2028
+
+Açais na garrafa + coca cola (todos inculem)
+
+ p1_:açai leite em pó, leite condensado 10,00
+ quantidade em estoque:300\validade:12.07.2028
+
+ p2_paçoca:açai, mousse de morango em pedaço leite em pó 20,00
+ quantidade em estoque:320\validade:17.04.2028
+
+ p3_sensação: açai, mousse de morango em oedaço, leite em pó 20,00
+ quantidade em estoque:400\validade:29.10.2028
+
+ p4_creme de avelã:açai, nutella, abacaxi 25,00
+ quantidade em estoque:420\validade:15.07.2028
+
+ p5_maraxi:açai, creme de maracujá, abacaxi, paçoca 30,00
+ quantidade em estoque:500\validade10.12.2028
+
+elif combos_de_açai:
+
+uma garrafa e um copo de 300ml(açai batido com banana)
+com adicionais de nutella e paçoca creme de maracujá 30,00
+dois açais premium 500ml açai, nutella, creme de morango, confetes) 35,00
+
+
+if:Barcas_de_açai🚤
+
+uma garrafa e um  copo de 300ml(açai batido com banana)
+com adicionais de nutella e paçoca, creme de maracujá 30,00
+dois açias premium 500 ml açai, nutella, creme de morango, confeites) 35,00
+
+   if:Barcas_de_açai
+     Especial + 5 acomp.+ adicionais(ideal para 4 a 7,pessoas)50,00+ água
+     quantidade no estoque:400\ data de val:09.03.2027
+
+     Simpes + 3 acomp.(ideia para 2 pessoas)40,00
+     quantidade no estoque:550\ data de val:02.12🍨
+
+ elif opção_definida=0:
+
+elif opcao=='4':
+
+tradicionalzao----
+qtd_venda =int(int(f'quantas unidades de]p1 tradicionalzao'70 qtd))
+
+paçoca----
+qtd_venda=int(int(f'quantas unidades de]p2 paçoca'60 qtd))
+
+dois açais premium----
+qtd_venda=int(int(f'quantas unidades de]p3 dois açais premium'80 qtd))
+
